293b83479c7b57f25f492d9a6a49eab44806ece5
DuVaaL <46516806+DuVaaL@users.noreply.github.com>
2020-04-13 11:31:15 +0100

    Update CreateDialog.js

--- src/components/CreateDialog.js
+++ src/components/CreateDialog.js
@@ -3,6 +3,8 @@ import {Dialog, Button } from '@material-ui/core'
 import {DialogTitle, DialogContent, DialogContentText} from '@material-ui/core' ;
 import AddIcon from '@material-ui/icons/Add';
 import Form from './Form';
+import {Consumer} from '../Context';
+
 
 class CreateDialog extends Component {
     state = {
@@ -23,33 +25,37 @@ class CreateDialog extends Component {
 
    
     render(){
-        const { open } = this.state,
-              { muscles } = this.props ;
-                
+        const { open } = this.state;
+             
         return (
-            <Fragment>
-                     <Button variant="fab" color="secondary"
-                      onClick={this.handleToggle} 
-                      style={{ position: "absolute", right: 10 }} >
-                         <AddIcon />
-                     </Button>
-                <Dialog open={open} onClose={this.handleToggle} >
-                    <DialogTitle >
-                        Create New Exercice
-                    </DialogTitle>
-                      <DialogContent>
-                            <DialogContentText>
-                            Please fill out the form below 
-                            </DialogContentText>
+            <Consumer>
+                {
+                    ({ muscles }) => <Fragment>
+                        <Button variant="fab" color="secondary"
+                        onClick={this.handleToggle} 
+                        style={{ position: "absolute", right: 10 }} >
+                            <AddIcon />
+                        </Button>
+                    <Dialog open={open} onClose={this.handleToggle} >
+                        <DialogTitle >
+                            Create New Exercice
+                        </DialogTitle>
+                        <DialogContent>
+                                <DialogContentText>
+                                Please fill out the form below 
+                                </DialogContentText>
 
-                            <Form 
-                                muscles={muscles}
-                                onSubmit={this.handleFormSubmit}
-                            />
+                                <Form 
+                                    muscles={muscles}
+                                    onSubmit={this.handleFormSubmit}
+                                />
 
-                        </DialogContent>
-                </Dialog>
-            </Fragment>
+                            </DialogContent>
+                        </Dialog>
+                    </Fragment>
+                }
+                    
+            </Consumer>
         )
     }
 }
